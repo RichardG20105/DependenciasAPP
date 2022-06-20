3805103f68467085f20c92610e46954a34533e1f
Richard Gualancañay <richard20105@hotmail.com>
2022-06-20 17:17:46 -0500

    Aumento en las imagenes para las categorias

--- DependenciasAPP/src/componentes/Iconos.tsx
+++ DependenciasAPP/src/componentes/Iconos.tsx
@@ -3,34 +3,34 @@ export const getIconoInicio = (id:number) => {
     switch(id){
         case 1:
             return require(path+'auditorio.png')
-            break;
         case 2:
             return require(path+'servicioh.png')
-            break;
         case 3:
             return require(path+'estadio.png')
-            break;
         case 4: 
             return require(path+'cancha.png');
-            break;
         case 5:
             return require(path+'parqueadero.png')
-            break;
         case 6:
             return require(path+'edificio-de-oficinas.png')
-            break;
         case 7:
             return require(path + 'biblioteca.png')
-            break
         case 8:
             return require(path + 'cafeteria.png')
-            break
         case 9:
-            return require(path + 'edificio-de-oficinas.png')
-            break
+            return require(path+'edificio-de-oficinas.png')
+        case 10:
+            return require(path + 'piscina.png')
+        case 11:
+            return require(path + 'coliseo.png')
+        case 12:
+            return require(path + 'gimnasio.png')
+        case 13:
+            return require(path + 'unidad-academica.png')
+        case 14:
+            return require(path + 'centro-medico.png')            
         default:
             return
-            break;
     }
 }
 
@@ -39,28 +39,20 @@ export const getIconoMapa = (id:number) => {
     switch(id){
         case 1:
             return require(path+'Auditorio.png')
-            
         case 2:
             return require(path+'Baño.png')
-           
         case 3:
             return require(path+'Estadio.png')
-            
-        case 4: 
+        case 4:
             return require(path+'Cancha.png');
-            
         case 5:
             return require(path+'Parqueadero.png')
-            
         case 6:
             return require(path+'Facultad.png')
-            
         case 7:
             return require(path+'Biblioteca.png')
-            
         case 8:
             return require(path+'Cafeteria.png')
-            
         case 9:
             return require(path+'Administrativo.png')
         case 10:
@@ -83,35 +75,24 @@ export const getColorLetras = (id:number) => {
     switch(id){
         case 1:
             return '#7c2424'
-            break;
         case 2:
             return '#0c7c8c'
-            break;
         case 3:
             return '#3c8c04'
-            break;
         case 4:
             return '#6c7454'
-            break;
         case 5:
             return '#0434bc'
-            break;
         case 6:
             return '#641c0c'
-            break;
         case 7:
             return '#e4c404'
-            break;
         case 8:
             return '#543404'
-            break;
         case 9:
             return '#4c4c4c'
-            break;
         default:
             return '#d40404'
-            break;
-
     }
 }
 
@@ -119,33 +100,33 @@ export const getTipoDependencias = (id: number) => {
     switch(id){
         case 1:
             return 'Auditorio'
-            break;
         case 2:
             return 'Baño'
-            break;
         case 3:
             return 'Estadio'
-            break;
         case 4:
             return 'Cancha'
-            break;
         case 5:
             return 'Parqueadero'
-            break;
         case 6:
             return 'Facultad'
-            break;
         case 7:
             return 'Biblioteca'
-            break;
         case 8:
             return 'Cafeteria'
-            break;
         case 9:
             return 'Administrativo'
-            break;
+        case 10:
+            return 'Piscina'
+        case 11:
+            return 'Coliseo'
+        case 12:
+            return 'Gimnasio'
+        case 13:
+            return 'Unidad Academica'
+        case 14:
+            return 'Centro Médico'
         default:
             return ''
-            break;
     }
 } 
--- DependenciasAPP/src/hooks/TiposDependenciaUso.tsx
+++ DependenciasAPP/src/hooks/TiposDependenciaUso.tsx
@@ -17,7 +17,6 @@ export const TiposDependenciaUso = () => {
 
     return {
         TiposDependencia,
-        setTiposDependencia,
         CargarTiposDependencia,
     }
 }
